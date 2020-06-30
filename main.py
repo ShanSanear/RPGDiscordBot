d8d060fac3220355e9533b2919e2334a2e7a0f61
Mateusz Pruski <pruskimateusz93@gmail.com>
2020-06-30 17:47:49 +0200

    Added usage of logger.

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import toml
 from discord import TextChannel, VoiceState, VoiceChannel, Member
 from discord.ext import commands
 
+from dnd5_api import DnD5Api
+from loggers import create_loggers, general_logger
 from music import Music
 
 
@@ -14,24 +16,21 @@ class MyBot(commands.Bot):
         self.config = config
 
     async def on_ready(self):
-        print('Logged in as')
-        print(self.user.name)
-        print(self.user.id)
-        print('------')
+        general_logger.info("Logged in as: %s [ID: %d]", self.user.name, self.user.id)
 
     async def on_voice_state_update(self, member: Member, before: VoiceState, after: VoiceState):
-        voice_channel: VoiceChannel = self.get_channel(self.config['REMINDER']['VOICE_CHANNEL_ID'])
+        voice_channel: VoiceChannel = self.get_channel(self._config['REMINDER']['VOICE_CHANNEL_ID'])
         if voice_channel != after.channel:
             return
-        mg = self.get_user(self.config['REMINDER']['GM_ID'])
-        text_channel: TextChannel = self.get_channel(self.config['REMINDER']['TEXT_CHANNEL_ID'])
+        mg = self.get_user(self._config['REMINDER']['GM_ID'])
+        text_channel: TextChannel = self.get_channel(self._config['REMINDER']['TEXT_CHANNEL_ID'])
         if mg == member:
             await text_channel.send(f'Nie zapomnij o nagrywaniu, {member.mention}!')
         else:
             await text_channel.send(f"Nie zapomnij o celu krótkoterminowym, {member.mention}!")
 
     async def send_message_to_text_channel(self, message):
-        text_channel: TextChannel = self.get_channel(self.config['REMINDER']['TEXT_CHANNEL_ID'])
+        text_channel: TextChannel = self.get_channel(self._config['REMINDER']['TEXT_CHANNEL_ID'])
         await text_channel.send(message)
 
 
