a32e038e74d0f514c2b86fe6babf28a206eff355
Mateusz Pruski <pruskimateusz93@gmail.com>
2020-07-02 18:10:50 +0200

    Reformatted to use class instance formatting of message.

--- dnd5_api.py
+++ dnd5_api.py
@@ -25,6 +25,8 @@ class Resource:
     def __init__(self, name: str, api_subpath: str, fields_to_show: Tuple):
         self.name = name
         self._full_api_path = urljoin(self._base_api, api_subpath)
+        self.data = {}
+        self.fields_to_show = fields_to_show
 
     def _get_by_index(self, index):
         url = urljoin(self._full_api_path, index)
@@ -52,24 +54,31 @@ class Resource:
 
 class Spell(Resource):
 
-    def __init__(self, name:str, fields_to_show: Tuple):
+    def __init__(self, name: str, fields_to_show: Tuple):
         super().__init__(name, api_subpath="spells/", fields_to_show=fields_to_show)
+        self.start_of_the_message = "Found spell:"
 
+    def get_formatted_message(self):
+        fields = {'Name': self.data['name'],
+                  'Description': '\n'.join(self.data['desc']),
+                  "Components": ' '.join(self.data['components']),
+                  "Range": data['range']}
+        if "higher_level" in self.data:
+            fields["Higher_level"] = '\n'.join(self.data['higher_level'])
+        message = "\n".join((f"**{key}** : {value}" for key, value in fields.items()
+                             if self.fields_to_show is None or key in self.fields_to_show)
+                            )
+
+        return f"{self.start_of_the_message}\n{message}"
 
 
 class DnD5Api(commands.Cog):
     _classes = {
         "spell": Spell
     }
-    _message = {
-        "spell": "Found spell:"
-    }
 
     def __init__(self, bot: Bot):
         self.bot = bot
-
-    def get_formatted_message(self):
-        pass
 
     @staticmethod
     def format_message(start_of_the_message, data, fields_to_show: Union[None, Tuple, List] = None):
@@ -91,18 +100,17 @@ class DnD5Api(commands.Cog):
         :param what_for - what are you searching for (for now - only spell)
         :param name - name of the spell
         :param fields_to_show - space separated fields to show: Name, Description, Components, Range, Higher_level"""
-        if what_for not in self._classes or what_for not in self._message:
+        if what_for not in self._classes:
             raise NotImplementedError
         if not fields_to_show:
             fields_to_show = None
         general_logger.debug("Searching for: %s using name: %s", what_for, name)
         async with ctx.typing():
             cls = self._classes[what_for]
-            msg = self._message[what_for]
             try:
-                cls(name, fields_to_show).fetch_by_name()
-                message = ""
-                await ctx.send(self.format_message(msg, data, fields_to_show=fields_to_show))
-                # await ctx.send(json.dumps(data, indent=4))
+                search_instance = cls(name, fields_to_show)
+                await search_instance.fetch_by_name()
+                message = search_instance.get_formatted_message()
+                await ctx.send(message)
             except ResourceNotFound:
                 await ctx.send(f"Spell not found: {name}")
