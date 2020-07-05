de5ca947243b8524dee17582fe7ed46364546d6f
Mateusz Pruski <pruskimateusz93@gmail.com>
2020-07-05 08:09:27 +0200

    Slightly changed format of logging.

--- loggers.py
+++ loggers.py
@@ -6,7 +6,7 @@ general_logger = logging.getLogger("general_logger")
 def create_loggers(level=logging.DEBUG):
     handler = logging.StreamHandler()
     formatter = logging.Formatter(
-        '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+        '[%(asctime)s]:[%(name)-12s] [%(levelname).1s]: %(message)s', datefmt="%Y-%m-%d %H:%M:%S")
     handler.setFormatter(formatter)
     general_logger.addHandler(handler)
     general_logger.setLevel(level)
