ea917f9163e9b9408b516e82637a05eb457ecc83
Mateusz Pruski <pruskimateusz93@gmail.com>
2020-07-05 08:09:53 +0200

    Added usage of utils, simplified naming.

--- dnd5_api.py
+++ dnd5_api.py
@@ -1,32 +1,20 @@
-import json
-from typing import Union, Tuple, List
+from urllib.parse import urljoin
 
-import discord
+import requests
 from discord.ext import commands
 from discord.ext.commands import Bot
 
 from loggers import general_logger
-from urllib.parse import urljoin
-
-import requests
-
-
-class ResourceNotFound(Exception):
-    pass
-
-
-class SpellNotFound(ResourceNotFound):
-    pass
+from utils import flyweight, ResourceNotFound
 
 
 class Resource:
     _base_api = "https://www.dnd5eapi.co/api/"
 
-    def __init__(self, name: str, api_subpath: str, fields_to_show: Tuple):
+    def __init__(self, name: str, api_subpath: str):
         self.name = name
         self._full_api_path = urljoin(self._base_api, api_subpath)
-        self.data = {}
-        self.fields_to_show = fields_to_show
+        self.response_data = {}
 
     def _get_by_index(self, index):
         url = urljoin(self._full_api_path, index)
@@ -36,6 +24,9 @@ class Resource:
         return response.json()
 
     def fetch_by_name(self):
+        if self.response_data:
+            general_logger.debug("Using cached data for: %r", self)
+            return
         name_query = f"?name={self.name}"
         url = urljoin(self._full_api_path, name_query)
         general_logger.debug("Url: %s", url)
@@ -43,30 +34,38 @@ class Resource:
         response.raise_for_status()
         returned = response.json()
         if returned['count'] == 1:
-            return self._get_by_index(returned['results'][0]['index'])
+            self.response_data = self._get_by_index(returned['results'][0]['index'])
         elif returned['count'] > 1:
             names = {result['name']: result['index'] for result in returned['results']}
             if self.name not in names:
                 raise ResourceNotFound
-            return self._get_by_index(names[self.name])
-        raise ResourceNotFound
+            self.response_data = self._get_by_index(names[self.name])
+        else:
+            raise ResourceNotFound
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.name}')"
+
+    @property
+    def already_fetched(self):
+        return True if self.response_data else False
 
 
+@flyweight
 class Spell(Resource):
-
-    def __init__(self, name: str, fields_to_show: Tuple):
-        super().__init__(name, api_subpath="spells/", fields_to_show=fields_to_show)
+    def __init__(self, name: str):
+        super().__init__(name, api_subpath="spells/")
         self.start_of_the_message = "Found spell:"
 
-    def get_formatted_message(self):
-        fields = {'Name': self.data['name'],
-                  'Description': '\n'.join(self.data['desc']),
-                  "Components": ' '.join(self.data['components']),
-                  "Range": data['range']}
-        if "higher_level" in self.data:
-            fields["Higher_level"] = '\n'.join(self.data['higher_level'])
+    def get_formatted_message(self, fields_to_show):
+        fields = {'Name': self.response_data['name'],
+                  'Description': '\n'.join(self.response_data['desc']),
+                  "Components": ' '.join(self.response_data['components']),
+                  "Range": self.response_data['range']}
+        if "higher_level" in self.response_data:
+            fields["Higher_level"] = '\n'.join(self.response_data['higher_level'])
         message = "\n".join((f"**{key}** : {value}" for key, value in fields.items()
-                             if self.fields_to_show is None or key in self.fields_to_show)
+                             if fields_to_show is None or key in fields_to_show)
                             )
 
         return f"{self.start_of_the_message}\n{message}"
@@ -80,26 +79,12 @@ class DnD5Api(commands.Cog):
     def __init__(self, bot: Bot):
         self.bot = bot
 
-    @staticmethod
-    def format_message(start_of_the_message, data, fields_to_show: Union[None, Tuple, List] = None):
-        fields = {'Name': data['name'],
-                  'Description': '\n'.join(data['desc']),
-                  "Components": ' '.join(data['components']),
-                  "Range": data['range']}
-        if "higher_level" in data:
-            fields["Higher_level"] = '\n'.join(data['higher_level'])
-        message = "\n".join((f"**{key}** : {value}" for key, value in fields.items()
-                             if fields_to_show is None or key in fields_to_show)
-                            )
-
-        return f"{start_of_the_message}\n{message}"
-
     @commands.command()
     async def search(self, ctx, what_for, name, *fields_to_show):
         """Searches for spell (for now) using it's name.
         :param what_for - what are you searching for (for now - only spell)
         :param name - name of the spell
-        :param fields_to_show - space separated fields to show: Name, Description, Components, Range, Higher_level"""
+        :param fields_to_show - space separated fields to show; Name, Description, Components, Range, Higher_level"""
         if what_for not in self._classes:
             raise NotImplementedError
         if not fields_to_show:
@@ -108,9 +93,9 @@ class DnD5Api(commands.Cog):
         async with ctx.typing():
             cls = self._classes[what_for]
             try:
-                search_instance = cls(name, fields_to_show)
-                await search_instance.fetch_by_name()
-                message = search_instance.get_formatted_message()
+                search_instance = cls(name)
+                search_instance.fetch_by_name()
+                message = search_instance.get_formatted_message(fields_to_show)
                 await ctx.send(message)
             except ResourceNotFound:
                 await ctx.send(f"Spell not found: {name}")
