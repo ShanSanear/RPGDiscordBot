7038752061eb878fbcd52cfaad476f8d2831b583
Mateusz Pruski <pruskimateusz93@gmail.com>
2020-07-05 08:10:17 +0200

    Extracting maintain related commands.

---
+++ maintain.py
@@ -0,0 +1,25 @@
+from discord.ext import commands
+from discord.ext.commands import Bot
+
+from rpg_discord_bot import RPGDiscordBot
+
+
+class Maintain(commands.Cog):
+    def __init__(self, bot: RPGDiscordBot):
+        self.bot = bot
+
+    @commands.command()
+    async def join(self, ctx, *, channel: discord.VoiceChannel):
+        """Joins a voice channel"""
+
+        if ctx.voice_client is not None:
+            return await ctx.voice_client.move_to(channel)
+
+        await channel.connect()
+
+    @commands.command()
+    @commands.is_owner()
+    async def shutdown(self, ctx):
+        await self.bot.send_message_to_text_channel("Turning off")
+        await ctx.bot.logout()
+        await ctx.bot.close()
