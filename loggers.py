4e8a28dd40a455e4b8c14b5643fd36b0455c1c1f
Mateusz Pruski <pruskimateusz93@gmail.com>
2020-08-02 21:06:44 +0200

    Added file logging.

--- loggers.py
+++ loggers.py
@@ -1,14 +1,19 @@
 import logging
+import sys
 
 general_logger = logging.getLogger("general_logger")
 
 
 def create_loggers(level=logging.DEBUG):
-    handler = logging.StreamHandler()
+    stream_handler = logging.StreamHandler()
     formatter = logging.Formatter(
         "[%(asctime)s]:[%(name)-12s] [%(levelname).1s]: %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
     )
-    handler.setFormatter(formatter)
-    general_logger.addHandler(handler)
+    file_handler = logging.FileHandler(filename='RPGDiscordBot.log', mode='a')
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(formatter)
+    general_logger.addHandler(file_handler)
+    stream_handler.setFormatter(formatter)
+    general_logger.addHandler(stream_handler)
     general_logger.setLevel(level)
