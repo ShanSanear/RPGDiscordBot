f572ac7ecae5ad790b31098425a760ae918b6c10
Shan <pruskimateusz93@gmail.com>
2021-05-02 21:51:22 +0200

    Added docstring and changed name of the method.

--- maintain.py
+++ maintain.py
@@ -3,7 +3,7 @@ import traceback
 from datetime import datetime, timedelta
 
 import discord
-from discord import Member, TextChannel
+from discord import Member, TextChannel, Message
 from discord.ext import commands
 from discord.ext.commands import Context
 
@@ -12,7 +12,14 @@ from rpg_discord_bot import RPGDiscordBot
 
 
 class Maintain(commands.Cog):
+    """
+    Cog which handles simple maintain tasks (such as joining channel, disconnecting etc)
+    """
     def __init__(self, bot: RPGDiscordBot):
+        """
+        Constructor
+        :param bot: RPGDiscordBot instance
+        """
         self.bot = bot
         self._users_to_be_followed = {}
 
@@ -44,26 +51,50 @@ class Maintain(commands.Cog):
 
     @commands.command()
     @commands.is_owner()
-    async def clear_today_messages(self, ctx: Context, number):
+    async def clear_today_messages(self, ctx: Context, number: str):
+        """
+        Clears messages created by bot today. Requires number of messages
+        :param ctx: Message context
+        :param number: number of messages
+        """
         number = int(number)
         last_24_hours = datetime.utcnow() - timedelta(days=1)
         general_logger.info("Clearing messages from channel: %s since: %s", ctx.channel, last_24_hours)
-        await ctx.channel.purge(limit=number, check=self.check_for_author,
+        await ctx.channel.purge(limit=number, check=self.check_if_bot_is_author_of_message,
                                 after=last_24_hours)
 
-    def check_for_author(self, message):
+    def check_if_bot_is_author_of_message(self, message: Message):
+        """
+        Checks if bot is author of the message.
+        :param message: Message being checked
+        :return: True in case message has been created by bot or not
+        """
         general_logger.debug("Message: %s, author: %s", message, message.author)
         return message.author == self.bot.user
 
     @commands.command()
     async def follow_another_user(self, ctx, user_to_be_followed: Member, user_following: Member):
+        """
+        TODO:
+        Command which saves information about which user should follow who in voice channels
+        :param ctx: Message context
+        :param user_to_be_followed: Who should be followed
+        :param user_following: Who should be following
+        :return:
+        """
         general_logger.debug("User to be followed: %s, User following: %s", user_to_be_followed, user_following)
         await ctx.send("Following...")
 
 
 class CommandErrorHandler(commands.Cog):
-
+    """
+    Makes it easier to handle command errors.
+    """
     def __init__(self, bot):
+        """
+        Constructor
+        :param bot: RPGDiscordBot instance
+        """
         self.bot = bot
 
     @commands.Cog.listener()
