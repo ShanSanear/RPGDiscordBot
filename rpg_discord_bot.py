d2082e957063f6da9ddd776f0146c72d44f3f7d3
Shan <pruskimateusz93@gmail.com>
2021-05-02 21:34:26 +0200

    Adding docstring.

--- rpg_discord_bot.py
+++ rpg_discord_bot.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta
-from typing import List
+from typing import List, Iterator
 
 from discord import TextChannel, Member, VoiceState, VoiceChannel, Message
 from discord.ext import commands
@@ -8,9 +8,14 @@ from loggers import general_logger
 
 
 class RPGDiscordBot(commands.Bot):
-    def __init__(self, command_prefix, config_data):
+    def __init__(self, command_prefix: str, config_data: dict):
+        """
+        Constructor for main bot class.
+        :param command_prefix: prefix for commands
+        :param config_data: configuration loaded from TOML file
+        """
         super(RPGDiscordBot, self).__init__(command_prefix)
-        self._config = config_data
+        self._config: dict = config_data
         self._gm_id: int = self._config["REMINDER"]["GM_ID"]
         self._blacklisted: List[int] = self._config["REMINDER"]["BLACKLISTED_IDS"]
         self._text_channel_id: int = self._config["REMINDER"]["TEXT_CHANNEL_ID"]
@@ -19,13 +24,20 @@ class RPGDiscordBot(commands.Bot):
         self._others_message: str = self._config["REMINDER"]["OTHERS_MESSAGE"]
 
     async def on_ready(self):
+        """
+        Logging information when on_ready event is being called
+        """
         general_logger.info("Logged in as: %s [ID: %d]", self.user.name, self.user.id)
 
-    async def send_reminder_to_text_channel(self, message):
+    async def send_reminder_to_text_channel(self, message: str):
+        """
+        Send reminder to default text channel
+        :param message: message to be send
+        """
         text_channel: TextChannel = self.get_channel(self._text_channel_id)
-        last_12_hours = datetime.utcnow() - timedelta(hours=12)
-        last_messages = await text_channel.history(after=last_12_hours).flatten()
-        last_messages = (message.content for message in last_messages)
+        last_12_hours: datetime.date = datetime.utcnow() - timedelta(hours=12)
+        last_messages: Iterator[Message] = await text_channel.history(after=last_12_hours).flatten()
+        last_messages: Iterator[Message] = (message.content for message in last_messages)
         if message in last_messages:
             general_logger.debug("Message %s appeared in the last 12 hours, skipping", message)
             return
@@ -34,6 +46,12 @@ class RPGDiscordBot(commands.Bot):
     async def on_voice_state_update(
         self, member: Member, before: VoiceState, after: VoiceState
     ):
+        """
+        Being run on voice state update being detected
+        :param member: member which is being affected by voice state change
+        :param before: state before update
+        :param after: state after update
+        """
         voice_channel: VoiceChannel = self.get_channel(self._voice_channel_id)
         if voice_channel != after.channel:
             return
