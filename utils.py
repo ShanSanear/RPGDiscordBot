a1ef86cc997545f5f8a12264f2a67fb160922606
Shan <pruskimateusz93@gmail.com>
2021-05-02 21:55:20 +0200

    Added docstring for flyweight.

--- utils.py
+++ utils.py
@@ -1,4 +1,9 @@
 def flyweight(cls):
+    """
+    Flyweight class wrapper.
+    :param cls: class to flyweight.
+    :return: Check to make sure there are no duplicates of the classes.
+    """
     instances = dict()
     return lambda *args, **kwargs: instances.setdefault(
         (args, tuple(kwargs.items())), cls(*args, **kwargs)
