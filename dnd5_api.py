43d168981a2c46f30cafe8781d6a2daa7b6e853c
Shan <pruskimateusz93@gmail.com>
2021-05-02 21:45:13 +0200

    Added docstrings for DND5e API.

--- dnd5_api.py
+++ dnd5_api.py
@@ -1,22 +1,37 @@
+from typing import Iterator, Optional
 from urllib.parse import urljoin
 
 import requests
 from discord.ext import commands
-from discord.ext.commands import Bot
+from discord.ext.commands import Bot, Context
 
 from loggers import general_logger
+from rpg_discord_bot import RPGDiscordBot
 from utils import flyweight, ResourceNotFound
 
 
 class Resource:
+    """
+    Class which represents possible resource at dnd5eapi endpoint
+    """
     _base_api = "https://www.dnd5eapi.co/api/"
 
     def __init__(self, name: str, api_subpath: str):
+        """
+        Constructor
+        :param name: name of the resource
+        :param api_subpath: subpath for the api calls
+        """
         self.name = name
         self._full_api_path = urljoin(self._base_api, api_subpath)
         self.response_data = {}
 
-    def _get_by_index(self, index):
+    def _get_by_index(self, index: str):
+        """
+        Get resource by provided index.
+        :param index: Index of item that has been requested
+        :return: Json response containing resource
+        """
         url = urljoin(self._full_api_path, index)
         general_logger.debug("Url: %s", url)
         response = requests.get(url)
@@ -24,6 +39,9 @@ class Resource:
         return response.json()
 
     def fetch_by_name(self):
+        """
+        Fetches object, using provided name - saves this state in the class itself.
+        """
         if self.already_fetched:
             general_logger.debug("Using cached data for: %r", self)
             return
@@ -49,16 +67,33 @@ class Resource:
 
     @property
     def already_fetched(self):
+        """
+        Was this data already fetched
+        :return: True if that data was already fetched, false otherwise
+        """
         return True if self.response_data else False
 
 
 @flyweight
 class Spell(Resource):
+    """
+    Spell resource from api
+    """
+
     def __init__(self, name: str):
+        """
+        Constructor.
+        :param name: Name of the spell to check
+        """
         super().__init__(name, api_subpath="spells/")
         self.start_of_the_message = "Found spell:"
 
-    def get_formatted_message(self, fields_to_show):
+    def get_formatted_message(self, fields_to_show: Optional[Iterator[str]]):
+        """
+        Creates already formatted message based on the fields to be shown requested by the user
+        :param fields_to_show: Fields to be shown
+        :return: Formatted text, ready to be placed as message in Discord
+        """
         fields = {
             "Name": self.response_data["name"],
             "Description": "\n".join(self.response_data["desc"]),
@@ -85,14 +120,22 @@ class Spell(Resource):
 
 
 class DnD5Api(commands.Cog):
+    """
+    Cog representing access to the DND5E API
+    """
     _classes = {"spell": Spell}
 
-    def __init__(self, bot: Bot):
+    def __init__(self, bot: RPGDiscordBot):
+        """
+        Constructor
+        :param bot: RPGDiscordBot instance
+        """
         self.bot = bot
 
     @commands.command()
-    async def search(self, ctx, what_for, name, *fields_to_show):
+    async def search(self, ctx: Context, what_for: str, name: str, *fields_to_show: Iterator[str]):
         """Searches for spell (for now) using it's name.
+        :param ctx - message context
         :param what_for - what are you searching for (for now - only spell)
         :param name - name of the spell
         :param fields_to_show - space separated fields to show; Name, Description, Components, Range, Higher_level"""
