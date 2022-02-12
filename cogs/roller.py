800a61c4aa4dbc140a9def74ff732a1b9954a78f
Shan <pruskimateusz93@gmail.com>
2022-02-12 16:14:24 +0100

    Made roller slightly more sophisticated

--- cogs/roller.py
+++ cogs/roller.py
@@ -1,5 +1,8 @@
+from __future__ import annotations
+
 import random
 import re
+from dataclasses import dataclass
 from typing import List
 
 from discord.ext import commands
@@ -10,10 +13,45 @@ from rpg_discord_bot import RPGDiscordBot
 ROLL_RE = re.compile(r"(?P<NumberOfDices>\d+)?[dkDK](?P<DiceSize>\d+)")
 
 
+@dataclass
+class RollResult:
+    entry: str
+    results: list[int]
+
+    @property
+    def complete_result(self):
+        return sum(self.results)
+
+    def __str__(self):
+        if len(self.results) > 1:
+            return f"**{self.complete_result}** ({'+'.join([str(value) for value in self.results])}) [{self.entry}]"
+        else:
+            return f"**{self.complete_result}** [{self.entry}]"
+
+    def __repr__(self):
+        return str(self)
+
+    def __radd__(self, other: int):
+        return other + self.complete_result
+
+
+def get_roll_result(roll_entry: re.Match):
+    try:
+        number_of_dices = int(roll_entry['NumberOfDices'])
+    except TypeError:
+        number_of_dices = 1
+    dice_size: int = int(roll_entry['DiceSize'])
+    return RollResult(
+        entry=f"{number_of_dices}d{dice_size}",
+        results=[random.randint(1, dice_size) for _ in range(number_of_dices)]
+    )
+
+
 class Roller(commands.Cog):
     """
     Simple roller Cog
     """
+
     def __init__(self, bot: RPGDiscordBot):
         """
         Constructor
@@ -28,26 +66,16 @@ class Roller(commands.Cog):
         :param ctx: Context
         :param rolls_to_roll Rolls to roll in format of NumberOfDices[dk]DiceSize, can provide multiple rolls split using spaces
         """
-        roll_results: List[tuple[str, int]] = []
+        roll_results: List[RollResult] = []
 
         for to_roll in rolls_to_roll:
             to_be_rolled = ROLL_RE.match(to_roll)
             if not to_be_rolled:
                 await ctx.send(f"'{to_be_rolled}' has not been detected as roll")
                 continue
-            try:
-                number_of_dices = int(to_be_rolled.group('NumberOfDices'))
-            except TypeError:
-                number_of_dices = 1
-            dice_size = int(to_be_rolled.group('DiceSize'))
-            current_roll_result = random.randint(number_of_dices, dice_size * number_of_dices + 1)
-            roll_results.append((to_be_rolled.group(0), current_roll_result))
+            roll_results.append(get_roll_result(to_be_rolled))
 
         loggers.general_logger.debug("Roll results: %s", roll_results)
 
-        rolls_with_values = []
-        final_value = 0
-        for roll_name, roll_result in roll_results:
-            rolls_with_values.append(f"{roll_result} ({roll_name})")
-            final_value += roll_result
-        await ctx.send(f"Your roll results: {' + '.join(rolls_with_values)} = {final_value}")
+        await ctx.send(
+            f"Your roll results: {' + '.join(str(result) for result in roll_results)} = **{sum(roll_results)}**")
