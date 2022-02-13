bada4ec183e7bf10d386cdd897fbf84bc6d93d47
Shan <pruskimateusz93@gmail.com>
2022-02-13 19:16:53 +0100

    Better division of commands, making it more obvious what is going on

--- cogs/stream.py
+++ cogs/stream.py
@@ -23,48 +23,129 @@ class Stream(commands.Cog):
         self._obs_endpoint = urljoin(self.bot.streaming_api_endpoint, "obs/")
         self._obs_on = False
         self._stream_on = False
+        self._recording_on = False
         self._obs_response = {}
 
     def _call_api_get(self, endpoint):
         pass
 
-    @commands.group(pass_context=True)
-    async def stream(self, ctx: Context):
+    @commands.is_owner()
+    @commands.group(pass_context=True, invoke_without_command=True)
+    async def obs(self, ctx: Context):
         """
-        Manage stream, get help to find out more
+        OBS related commands, check help for more info
         :param ctx:
+        :return:
         """
         pass
 
+    @commands.is_owner()
+    @obs.command(pass_context=True)
+    async def start(self, ctx: Context):
+        """
+        Start OBS application on recording machine
+        """
+        await self.run_obs_if_turned_off(ctx)
+
+    @commands.is_owner()
+    @obs.command(pass_context=True)
+    async def stop(self, ctx: Context):
+        """
+        Stop OBS application on recording machine
+        """
+        await self.turn_off_obs_if_turned_on(ctx)
+
+    @commands.is_owner()
+    @obs.group(pass_context=True, invoke_without_command=True)
+    async def record(self, ctx: Context):
+        """
+        Manage OBS recording, check help for more info
+        """
+
+    @commands.is_owner()
+    @record.command(pass_context=True)
+    async def start(self, ctx: Context):
+        """
+        Start OBS recording
+        """
+        if not self._obs_on:
+            general_logger.info("OBS is not turned on")
+            await ctx.send("OBS is not turned on")
+            return
+        general_logger.info("Starting recording...")
+        await self._start_obs_recording()
+        self._recording_on = True
+        await ctx.send("Starting OBS **recording**")
+
+    @commands.is_owner()
+    @record.command(pass_context=True)
+    async def stop(self, ctx: Context):
+        """
+        Stop OBS recording
+        """
+        general_logger.info("Stopping recording...")
+        if not self._obs_on:
+            general_logger.warning("Obs is not turned on")
+            await ctx.send("OBS is not turned on")
+            return
+        if not self._recording_on:
+            general_logger.warning("Recording is not turned on")
+            await ctx.send("Recording is not turned on")
+            return
+        await self._stop_obs_recording()
+        await ctx.send("Stopped OBS **recording**")
+
+    @commands.is_owner()
+    @obs.group(pass_context=True, invoke_without_command=True)
+    async def stream(self, ctx: Context):
+        """
+        Manage OBS stream, check help for more info
+        """
+        pass
+
+    @commands.is_owner()
     @stream.command(pass_context=True)
     async def start(self, ctx: Context):
         """
-        Start stream
+        Start OBS stream
         """
-        general_logger.info("Starting OBS stream...")
+        if not self._obs_on:
+            general_logger.info("OBS is not turned on")
+            await ctx.send("OBS is not turned on")
+            return
+        await self._start_obs_stream()
+        self._stream_on = True
+        await ctx.send("Starting OBS **stream**")
+
+    async def run_obs_if_turned_off(self, ctx):
         if not self._obs_on:
             self._obs_response = await self._turn_on_obs()
             await ctx.send("Waiting for OBS to be turned on...")
             await asyncio.sleep(5)
-        await self._start_obs_stream()
-        self._stream_on = True
-        await ctx.send("Starting **recording** (DEBUG)")
 
+    async def turn_off_obs_if_turned_on(self, ctx):
+        if self._obs_on:
+            self._obs_response = await self._turn_off_obs()
+
+    @commands.is_owner()
     @stream.command(pass_context=True)
     async def stop(self, ctx: Context):
         """
-        Stop stream
+        Stop OBS stream
         """
         if not self._obs_on:
             general_logger.warning("Obs is not turned on")
+            await ctx.send("OBS is not turned on")
             return
         if not self._stream_on:
             general_logger.warning("Stream is not turned on")
+            await ctx.send("Stream is not turned on")
             return
         general_logger.info("Stopping OBS stream...")
-        await ctx.send("Stopping **recording** (DEBUG)")
         await self._stop_obs_stream()
+        await ctx.send("Stopped OBS **stream**")
         await self._turn_off_obs()
+        await ctx.send("OBS Turned off")
 
     async def _turn_on_obs(self) -> dict:
         """
@@ -86,11 +167,20 @@ class Stream(commands.Cog):
         self._obs_on = False
         self._obs_response = {}
         general_logger.info("Turn off OBS response: %s", response.json())
+        return response.json()
 
     async def _start_obs_stream(self):
-        general_logger.info("Starting recording...")
-        call_endpoint_post(endpoint=urljoin(self._obs_endpoint, "recording/start"), )
+        general_logger.info("Starting stream...")
+        call_endpoint_post(endpoint=urljoin(self._obs_endpoint, "stream/start"), )
 
     async def _stop_obs_stream(self):
+        general_logger.info("Stopping stream...")
+        call_endpoint_post(endpoint=urljoin(self._obs_endpoint, "stream/stop"), )
+
+    async def _start_obs_recording(self):
+        general_logger.info("Starting recording...")
+        call_endpoint_post(endpoint=urljoin(self._obs_endpoint, "recording/start"))
+
+    async def _stop_obs_recording(self):
         general_logger.info("Stopping recording...")
-        call_endpoint_post(endpoint=urljoin(self._obs_endpoint, "recording/stop"), )
+        call_endpoint_post(endpoint=urljoin(self._obs_endpoint, "recording/start"))
